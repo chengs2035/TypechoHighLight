12e7d10a83a48358065ddcfd8681638ee5d489db
djc8 <chensongguo@gmail.com>
2020-12-10 15:26:03 +0800

    修复down-btn的问题

--- typechohight.js
+++ typechohight.js
@@ -13,15 +13,21 @@ $("pre code").each(function() {
 	this.id = "pre_code_" + (i++);
 	$(this).after('<div class="div-copy-code">'
 		+'<button class="btn-copy-code" data-clipboard-action="copy" data-clipboard-target="#' + this.id + '">Copy</button>'
-		+'<button class="btn-copy-code btn-down-code-2-img" >Down2Img</button></div>');
+		+'<button class="btn-copy-code btn-down-code-2-img" down-pic-id="' + this.id + '">Down2Img</button></div>');
 });
 $("button.btn-down-code-2-img").click(function(){
-	domtoimage.toJpeg($(this).parent().parent().get(0), { quality: 0.95 }).then(function (dataUrl) {
+	
+	var codeNode=document.getElementById($(this).attr("down-pic-id"));
+	
+	$(codeNode).toggleClass("take-pic");
+	//$("#text").attr("data_obj");
+	domtoimage.toJpeg(codeNode, { quality: 0.95 }).then(function (dataUrl) {
         var link = document.createElement('a');
         link.download = 'code.jpg';
         link.href = dataUrl;
 		link.click();
 		link=null;
+		$(codeNode).toggleClass("take-pic");
 	});
 	//domtoimage.toPng(());
 });
